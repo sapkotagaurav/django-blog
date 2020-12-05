10db6c5b63e2b13206337ae2477f668e24e76695
Gaurab Sapkota <gaurabsapkota906@gmail.com>
2020-12-05 19:38:13 +0545

    Shit

--- crogaurabsapkota/settings.py
+++ crogaurabsapkota/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -136,7 +136,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'iamgvs906@gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'hphuaweiacergoogle'
+EMAIL_HOST_PASSWORD = config('E_password')
+
 
 CKEDITOR_CONFIGS = {
     'default': {
