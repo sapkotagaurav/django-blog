f357c280bd6520268bdffa1557e91bb57736df8a
Gaurab Sapkota <gauravsapkota906@gmail.com>
2020-12-11 23:33:28 +0545

    Contact Form

--- crogaurabsapkota/models.py
+++ crogaurabsapkota/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(unique=False, max_length=200, null=True)
     email = models.CharField(unique=False, max_length=200, null=True)
-    message = models.CharField(unique=false, max_length=1000, null=false)
+    message = models.CharField(unique=False, max_length=1000, null=False)
     is_done = models.BooleanField(default=False,null=False)
     created_on = models.DateTimeField( auto_now_add=True,)
 
