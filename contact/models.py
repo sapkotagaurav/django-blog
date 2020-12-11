d92d85423a9fad6f1f6e85f433e731a6371a955f
Gaurab Sapkota <gauravsapkota906@gmail.com>
2020-12-11 23:54:26 +0545

    Contact Form

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(unique=False, max_length=200, null=True)
+    email = models.CharField(unique=False, max_length=200, null=True)
+    message = models.CharField(unique=False, max_length=1000, null=False)
+    is_done = models.BooleanField(default=False,null=False)
+    created_on = models.DateTimeField( auto_now_add=True,)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
