927abde6edb9eccc4774f1bc92b77ac282acfaf3
Gaurab Sapkota <gauravsapkota906@gmail.com>
2020-12-11 23:45:38 +0545

    Contact Form

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.core.mail import send_mail, BadHeaderError
+from .chronicles.models import Category, Post
+from .models import Contact
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from django.core.mail import EmailMultiAlternatives
 
-# Create your views here.
+
+
+
+posts = Post.objects.filter(status=1).order_by('created_on')
+years = []
+
+for a in posts:
+    years.append(a.created_on.year)
+res = []
+for year in years:
+    if year not in res:
+        res.append(year)
+
+
+years = res
+
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        b =  Contact.objects.create(name=name,email=email,message=message)
+        b.save()
+
+        context = {'p': 1, 'posts': posts,
+                   'cats': Category.objects.all(), 'years': years}
+        return HttpResponse(template.render(context, request))
+
+
+    else:
+        context = {'p': 0, 'posts': posts,
+                   'cats': Category.objects.all(), 'years': years}
+        return HttpResponse(template.render(context, request))
+
