76c7b703b5328f99cba6fd886c6d505897dbb27d
Gaurav Sapkota <gaurabsapkota@protonmail.com>
2021-04-09 21:20:47 +0545

    final

--- chronicles/views.py
+++ chronicles/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from . import models
 
-posts = models.Post.objects.filter(status=1).order_by('created_on')
+posts = models.Post.objects.filter(status=1).order_by('created_on').reverse()
 years = []
 
 for a in posts:
@@ -30,7 +30,7 @@ class PostListAll(generic.ListView):
 def Categories(request, cate):
     template = loader.get_template('blogs/cat.html')
     posts = models.Post.objects.filter(status=1).filter(
-        category__category__contains=cate).order_by('created_on')
+        category__category__contains=cate).order_by('created_on').reverse()
     cats = models.Category.objects.all()
     return HttpResponse(template.render({'cat': cate, 'posts': posts, 'cats':cats,'years':years},request))
 
@@ -39,7 +39,7 @@ def Year(request, year):
     template = loader.get_template('blogs/cat.html')
     cats = models.Category.objects.all()
     posts = models.Post.objects.filter(status=1).filter(
-        created_on__year=year).order_by('created_on')
+        created_on__year=year).order_by('created_on').reverse()
     return HttpResponse(template.render({'cat': str(year), 'posts': posts, 'years':years,'cats':cats},request))
 
 
@@ -49,7 +49,7 @@ def Search(request):
     q = request.GET['q']
     posts = models.Post.objects.filter(status=1).filter(
         title__icontains=q).order_by('created_on') | models.Post.objects.filter(status=1).filter(
-        blog__icontains=q).order_by('created_on')
+        blog__icontains=q).order_by('created_on').reverse()
     return HttpResponse(template.render({'cat': "results for: "+q, 'posts': posts, 'years':years,'cats':cats},request))
 
 
