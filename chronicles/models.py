5b09999a7495e6bc6beeae3ecedcff86bbd4ca10
Gaurav Sapkota <gauravsapkota906@gmail.com>
2022-02-19 01:40:01 -0600

    Add login

--- chronicles/models.py
+++ chronicles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from django.utils.text import slugify
 
 STATUS =((0,"Draft"),(1,"Public"))
 
@@ -37,6 +38,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return
